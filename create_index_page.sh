4e39a22145f5829f00c569dfc011d9f6c1caaacc
Bilal Syed Hussain <bilalshussain@gmail.com>
2014-05-22 21:11:03 +0100

    script to create index page

---
+++ create_index_page.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Script to create the index page
+
+set -e
+set -x
+
+outdir="$( cd "$( dirname "$0" )" && pwd )";
+
+cat  > ${outdir}/index.html <<HTML
+<!DOCTYPE html>
+<html class="no-js">
+    <head>
+        <meta charset="utf-8">
+        <title>CSPlib Development Builds</title>
+    </head>
+    <body>
+        <ul>
+HTML
+
+
+for name in $(find . -depth 1 -type d \( !  -name .git \)); do
+	echo "            <li> <a href='${name}'/>${name}</li>" >> ${outdir}/index.html
+done
+
+
+cat  >> ${outdir}/index.html <<HTML
+        </ul>
+    </body>
+</html>
+HTML
+
+set +x
